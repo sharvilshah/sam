0e1b0e5c1e1c9f3f24ca7eabe3e83aa0e69f13f4
Sharvil Shah <sharvil@sharvilshah.com>
2024-04-15 16:19:10 +0530

    Initial Commit

---
+++ sam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sam
+//
+//  Created by Sharvil Shah on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sam/samApp.swift
@@ -0,0 +1,17 @@
+//
+//  samApp.swift
+//  sam
+//
+//  Created by Sharvil Shah on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct samApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
